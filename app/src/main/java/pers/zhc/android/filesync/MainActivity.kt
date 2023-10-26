4cc72486b1d09b23fab12c29650ad69d41daa6ae
Zhai Can <bczhc0@126.com>
2023-10-26 22:37:19 +0800

    request internet/storage permission on launch

--- app/src/main/java/pers/zhc/android/filesync/MainActivity.kt
+++ app/src/main/java/pers/zhc/android/filesync/MainActivity.kt
@@ -4,16 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import pers.zhc.android.filesync.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        if (!it) {
+            ToastUtils.show(this, R.string.permission_denied_toast)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val bindings = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
+
+        requestPermissionLauncher.launch(android.Manifest.permission.INTERNET)
+        requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        requestPermissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
         bindings.syncBtn.setOnClickListener {
 
