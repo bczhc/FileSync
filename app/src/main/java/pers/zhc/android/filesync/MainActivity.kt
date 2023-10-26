a294c2a2569b4484e45cc9cae29079757d87b73a
Zhai Can <bczhc0@126.com>
2023-10-26 15:35:08 +0800

    basic MainActivity layout

--- app/src/main/java/pers/zhc/android/filesync/MainActivity.kt
+++ app/src/main/java/pers/zhc/android/filesync/MainActivity.kt
@@ -1,12 +1,39 @@
 package pers.zhc.android.filesync
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import pers.zhc.android.filesync.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        ToastUtils.show(this, JNI.test().toString())
+        val bindings = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindings.root)
+
+        bindings.syncBtn.setOnClickListener {
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.config -> {
+                startActivity(Intent(this, ConfigActivity::class.java))
+            }
+
+            else -> {
+                return false
+            }
+        }
+        return true
     }
 }
